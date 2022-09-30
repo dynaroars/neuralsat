aea6e7305d445e92b87e5131342202dbfb7b742c
Hai <duongvanhai187@gmail.com>
2022-09-30 10:37:38 -0400

    x
    Former-commit-id: e16a658d363b36a9c5d29a04050d58b8a4e7a0b3
    Former-commit-id: f6472aeed3b16c59a0d9bfdb5215c15ae3e99f09
    Former-commit-id: dc49621a25406911ab1c938bf925b19fc0ec6b21 [formerly e0da8d6d09a87ad55d57b4f28a5c1a23da465e20]
    Former-commit-id: 72d4160ad6d5ce6a0d2f1a641e9c2ce7e541fe08
    Former-commit-id: 02be3b90b8f5678f6e3491dd92779640f767d34d
    Former-commit-id: 8e4201c475770b98c6579ae4bf8d78b8522f848d

--- RacPLL/benchmark/acasxu/script/run_property_6.sh
+++ RacPLL/benchmark/acasxu/script/run_property_6.sh
@@ -1,1 +1,1 @@
-python3 main.py --net benchmark/acasxu/nnet/ACASXU_run2a_1_1_batch_2000.nnet --spec benchmark/acasxu/spec/prop_6.vnnlib
+python3 main.py --net benchmark/acasxu/nnet/ACASXU_run2a_1_1_batch_2000.nnet --spec benchmark/acasxu/spec/prop_6.vnnlib --device cpu --dataset acasxu
--- RacPLL/benchmark/acasxu/script/run_property_9.sh
+++ RacPLL/benchmark/acasxu/script/run_property_9.sh
@@ -1,1 +1,1 @@
-python3 main.py --net benchmark/acasxu/nnet/ACASXU_run2a_3_3_batch_2000.nnet --spec benchmark/acasxu/spec/prop_9.vnnlib
+python3 main.py --net benchmark/acasxu/nnet/ACASXU_run2a_3_3_batch_2000.nnet --spec benchmark/acasxu/spec/prop_9.vnnlib --device cpu --dataset acasxu
