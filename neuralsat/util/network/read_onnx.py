3bc23b97518248cbf1705eb740a094cea0998baf
Hai <>
2023-01-13 11:25:30 -0500

    minors
    Former-commit-id: 7115e2b964a40c0b028eadfd59caaf5de572b9dc
    Former-commit-id: 1330393ca1f62fbfdf2a9ad2a4711303aa309ac9
    Former-commit-id: 7f648169b7cba4b5ea4b6a09b7f7b16cee9c877e [formerly 95f54881d05e5bbf595a79b3cb0fb42bf0dd7769]
    Former-commit-id: ee651c34ea24450f9c72665c7d89372b4fd679f2
    Former-commit-id: 29e105258a3ccd38a4b4398d032ddc139d7a8fdb
    Former-commit-id: 3c316eb4ebbb7699b9ebcd43fcb36d6e53ef10eb

--- neuralsat/util/network/read_onnx.py
+++ neuralsat/util/network/read_onnx.py
@@ -5,8 +5,7 @@ import math
 import onnx
 import gzip
 
-from .onnx2pytorch import ConvertModel, operations
-
+import onnx2pytorch
 
 class PyTorchModelWrapper(nn.Module):
 
@@ -101,7 +100,7 @@ def load_model_onnx(path, is_channel_last=False, force_convert=False):
     input_shape = tuple(d.dim_value for d in onnx_input_dims) if len(onnx_input_dims) > 1 else (1, onnx_input_dims[0].dim_value)
     output_shape = tuple(d.dim_value for d in onnx_output_dims) if len(onnx_output_dims) > 1 else (1, onnx_output_dims[0].dim_value)
     # input_shape = tuple(input_shape)
-    pytorch_model = ConvertModel(onnx_model)
+    pytorch_model = onnx2pytorch.ConvertModel(onnx_model)
 
     if force_convert or len(input_shape) <= 2:
         new_modules = []
@@ -132,29 +131,23 @@ def load_model_onnx(path, is_channel_last=False, force_convert=False):
     new_modules = []
     need_permute = False
     for mi, m in enumerate(modules):
-        if isinstance(m, operations.add.Add):
+        if isinstance(m, onnx2pytorch.operations.add.Add):
             # ResNet model. No need to convert to sequential.
             return pytorch_model, is_channel_last
         if isinstance(m, torch.nn.Conv2d):
             # Infer the output size of conv.
             conv_h, conv_w = conv_output_shape((conv_h, conv_w), m.kernel_size, m.stride, m.padding)
             conv_c = m.weight.size(0)
-        if isinstance(m, operations.reshape.Reshape):
+        if isinstance(m, onnx2pytorch.operations.reshape.Reshape):
             # Replace reshape with flatten.
             new_modules.append(nn.Flatten())
-            # May need to permute the next linear layer if the model was in NHWC format.
-            need_permute = True and is_channel_last
-        elif isinstance(m, torch.nn.Linear) and need_permute:
-            # The original model is in NHWC format and we now have NCHW format, so the dense layer's weight must be adjusted.
-            new_m = nn.Linear(in_features=m.in_features, out_features=m.out_features, bias=m.bias is not None)
-            new_m.weight.data.copy_(m.weight.view(m.weight.size(0), conv_h, conv_w, conv_c).permute(0, 3, 1, 2).contiguous().view(m.weight.size(0), -1))
-            new_m.bias.data.copy_(m.bias)
-            need_permute = False
-            new_modules.append(new_m)
         elif isinstance(m, torch.nn.ReLU) and mi == (len(modules)-1):
             # not add relu if last layer is relu
             pass
+        elif isinstance(m, onnx2pytorch.operations.constant.Constant):
+            pass
         else:
+            # print('add', m, type(m))
             new_modules.append(m)
 
     seq_model = nn.Sequential(*new_modules)
