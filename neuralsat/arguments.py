c6339884e16b4590d9d377438142b92316d27660
Hai <>
2023-01-30 19:23:30 -0500

    updates
    Former-commit-id: dbb31680cdea6cfd35c103256a69b684eaf5d501
    Former-commit-id: 94912f2efa2b0a5adb3977f703362cf4a17aca62
    Former-commit-id: 3f5ed2f52033cac5b07b3722b5e6a343bdab4a8f [formerly 0135ac613c91836297fc99e38105800e15c8bdc0]
    Former-commit-id: 803af4688f7538e47392bc6abe688b1e7b3e0717
    Former-commit-id: 3b8d32f6dc15fd2a52535d85cb03f0b9295fba3b
    Former-commit-id: 1ba9a673c3be5ad891ab4598e8a25cbc125d5290

--- neuralsat/arguments.py
+++ neuralsat/arguments.py
@@ -27,7 +27,7 @@ class Configuration:
 
         self.early_stop = True # stop when all branches are verified (for hidden splitting)
         self.print_progress = True  # print remaining unverified branches
-        self.attack = False
+        self.attack = True
         self.pre_verify_mip_refine = True
 
     def __getitem__(self, key):
