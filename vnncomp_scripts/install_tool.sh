201bcc7a286a898858f7e57ce7a728eb161ed60b
lli <lmax.qaz@gmail.com>
2023-06-18 02:15:51 -0400

    setup script
    Former-commit-id: aef3ef4250bded765740ad4d3502365a84f66e82
    Former-commit-id: 016cfbcf3fe4ba223fc8e4aacdd96d34221d59b5
    Former-commit-id: b10c3882183dbada63c804bb831b6fda6170bec8 [formerly 4f76327a2b68b170585bf18b769dd09e50567506] [formerly c89fc86e19bcfa7b064e7e03da5e1109517d91b4 [formerly 3beb3a8b508b523b7b910aeecf25510d36c0cc68]]
    Former-commit-id: 43fefb54d6115e4898a1568aef4a740ec255c898 [formerly 929a5a666b35e0a67ea8d05e2bc24b1dde69695b]
    Former-commit-id: 570342da6a1b0156846fc09bbe3f72df2a5b32ad
    Former-commit-id: afab8feed4d619a2722fadbf5bc627fe73454711
    Former-commit-id: 7a6484bb7be052b04149da1e12fedc11c8df0adc

--- vnncomp_scripts/install_tool.sh
+++ vnncomp_scripts/install_tool.sh
@@ -1,6 +1,18 @@
 TOOL_NAME=neuralsat
 VERSION_STRING=v1
-CONDA_HOME = ~/conda
+
+CONDA_HOME=~/conda
+CONDA=$CONDA_HOME/bin/conda
+
+NEURALSAT_CONDA_HOME=~/.conda/envs/neuralsat
+NEURALSAT_PY=$NEURALSAT_CONDA_HOME/bin/python
+
+NEURALSAT_HOME=$(dirname $(pwd))
+NEURALSAT_MAIN=$NEURALSAT_HOME/neuralsat/main.py
+
+echo "Neuralsat home is                           $NEURALSAT_HOME"
+echo "Conda will be installed to                  $CONDA_HOME"
+echo "Neuralsat's conda env will be installed at  $NEURALSAT_CONDA_HOME"
 
 # check arguments
 if [ "$1" != ${VERSION_STRING} ]; then
@@ -9,10 +21,40 @@ if [ "$1" != ${VERSION_STRING} ]; then
 fi
 
 
-echo "Installing $TOOL_NAME"
+echo "======= Installing $TOOL_NAME ======="
 
 
 wget -O conda.sh https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh
 chmod 755 conda.sh
-./conda.sh -p $CONDA_HOME
 
+# install conda
+rm -rf $CONDA_HOME
+./conda.sh -bf -p $CONDA_HOME
+rm ./conda.sh
+
+# install env
+echo "======= Installing conda env ======="
+$CONDA env remove -p $NEURALSAT_CONDA_HOME
+rm -rf $NEURALSAT_CONDA_HOME
+$CONDA  env create -p $NEURALSAT_CONDA_HOME -f ../env.yaml
+
+
+# setup alias
+echo "======= Adding alias ======="
+echo "neuralsat alias added"
+alias neuralsat='$NEURALSAT_PY $NEURALSAT_MAIN'
+sed -i '/neuralsat/d' ~/.bashrc
+echo "alias neuralsat='$NEURALSAT_PY $NEURALSAT_MAIN'" >> ~/.bashrc
+
+
+# install gurobi license
+echo "======= Installing Gurobi License ======="
+echo "enter WLSACCESSID"
+read ACCESSID
+echo "enter WLSSECRET"
+read SECRET
+echo "enter LICENSEID"
+read LICENSEID
+echo "WLSACCESSID=$ACCESSID" > ~/gurobi.lic
+echo "WLSSECRET=$SECRET" >> ~/gurobi.lic
+echo "LICENSEID=$LICENSEID" >> ~/gurobi.lic
