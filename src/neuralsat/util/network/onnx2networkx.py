d6ace97300804609a740e7d4eb7b4bedfe934c17
hocdot <duongvanhai187@gmail.com>
2025-07-14 20:40:53 -0400

    x

--- src/neuralsat/util/network/onnx2networkx.py
+++ src/neuralsat/util/network/onnx2networkx.py
@@ -134,10 +134,10 @@ def prepare_graph(net, input_shape, objective):
     layer_id = 1
     pre = torch.randn(input_shape)
     for layer in list(net.modules())[1:]: # TODO: update
-        print(f'Processing {layer=} {layer_id=}')
+        print(f'Processing {layer=} {layer_id=} n_layers={len(list(net.modules())[1:])}')
         post = layer(pre)
         if isinstance(layer, torch.nn.Linear):
-            if layer_id == len(list(net.modules())[1:]) - 2:
+            if layer == list(net.modules())[-1]:
                 cs = objective.cs
                 print(f'{cs=} {cs.shape=}')
                 
